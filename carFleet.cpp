758ce38f9b3806ba25a772db54e608797f5567a5
Pakin49 <philgus884@gmail.com>
2025-05-30 10:34:12 +0700

    carfleet

---
+++ carFleet.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<vector>
+#include<stack>
+
+using namespace std;
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        
+    }
+};
